f46f68faaa6041bdbdf9a11a32865447cd2c48b9
Bryan Ariana <arianabryan02@gmail.com>
2023-09-19 17:49:45 -0600

    Agregar archivos de proyecto.

---
+++ administrador/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿
+namespace administrador
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(635, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 63);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Actualizar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.button2.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(635, 154);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 60);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Detener";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridView1.GridColor = System.Drawing.Color.BlueViolet;
+            this.dataGridView1.Location = new System.Drawing.Point(40, 63);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(556, 500);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Nombre";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 93;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "ID";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 53;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "CPU";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 65;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Memoria";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Width = 98;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button3.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(635, 520);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 42);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Salir";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label1.Font = new System.Drawing.Font("Arial Black", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(40, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(431, 40);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Administrador de Procesos";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.OldLace;
+            this.ClientSize = new System.Drawing.Size(760, 595);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ administrador/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace administrador
+{
+    public partial class Form1 : Form
+    {
+        private Process selectedProcess; // Almacena el proceso seleccionado
+        private PerformanceCounter cpuCounter; // Contador de uso de CPU
+        //private int selectedProcessId = -1;
+        public Form1()
+        {
+            InitializeComponent();
+      
+            cpuCounter = new PerformanceCounter("Process", "% Processor Time", Process.GetCurrentProcess().ProcessName);
+
+        }
+
+        // Metodo que se ejecuta al cargar el formulario
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            RefreshProcessList(); // Actualiza la lista de procesos
+            timer1.Start(); // Inicia el temporizador para actualización automática
+        }
+
+        // Metodo para actualizar la lista de procesos en el DataGridView
+        private void RefreshProcessList()
+        {
+            Process[] processes = Process.GetProcesses();
+            dataGridView1.Rows.Clear(); // Limpia las filas existentes en el DataGridView
+
+            foreach (var process in processes)
+            {
+                try
+                {
+                    // Obtiene el uso de CPU y la memoria del proceso
+                    var cpuUsage = GetCpuUsage(process);
+                    var memoryUsage = GetMemoryUsage(process);
+
+                    // Agrega una fila con información del proceso al DataGridView
+                    dataGridView1.Rows.Add(
+                        process.ProcessName,
+                        process.Id.ToString(),
+                        cpuUsage.ToString("0.00") + "%",
+                        memoryUsage.ToString("0.00") + "MB"
+                    );
+                }
+                catch (Exception ex)
+                {
+                    //MessageBox.Show("Error al obtener el uso de CPU: " + ex.Message);
+                }
+            }
+        }
+
+        // Metodo para obtner el uso de memoria de un proceso en mb
+        private double GetMemoryUsage(Process process)
+        {
+            return process.WorkingSet64 / (1024.0 * 1024.0); // Convierte a megabytes
+        }
+
+        // Metodo para obtener el uso de CPU de un proceso en porcentaje
+        private double GetCpuUsage(Process process)
+        {
+           
+            var startTime = DateTime.UtcNow.AddMilliseconds(-process.TotalProcessorTime.TotalMilliseconds);
+            var currentTime = DateTime.UtcNow;
+            var cpuUsage = 100.0 * (process.TotalProcessorTime.TotalMilliseconds / (currentTime - startTime).TotalMilliseconds);
+
+            return cpuUsage;
+        }
+
+        // Boton para detener un proceso
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                var selectedRow = dataGridView1.SelectedRows[0];
+                var processId = int.Parse(selectedRow.Cells[1].Value.ToString());
+
+                try
+                {
+                    // Obtiene el proceso seleccionado y lo detiene
+                    Process process = Process.GetProcessById(processId);
+                    process.Kill();
+                    RefreshProcessList(); //Actualiza la lista de procesos
+                }
+                catch (Exception ex)
+                {
+                    //MessageBox.Show("Error al detener el proceso: " + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Selecciona un proceso para detenerlo.");
+            }
+        }
+
+        //  Botón para actualizar la lista de procesos
+        private void button1_Click(object sender, EventArgs e)
+        {
+            {
+                RefreshProcessList();
+            }
+        }
+
+        // Metodo del temporizador para actualización automática
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            RefreshProcessList(); // Actualiza la lista de procesos
+            if (selectedProcess != null) // Actualiza el uso de CPU del proceso seleccionado
+            {
+                try
+                {
+                    
+                    var cpuUsage = GetCpuUsage(selectedProcess);
+                   
+                }
+                catch
+                {
+                    
+                 //   progressBar1.Value = 0;
+                   
+                }
+            }
+            else
+            {
+                
+               // progressBar1.Value = 0;
+                
+            }
+        }
+
+        // Metodo para selección de una fila en el DataGridView
+        private void dataGridView1_SelectionChange(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                var selectedRow = dataGridView1.SelectedRows[0];
+                var processId = int.Parse(selectedRow.Cells[1].Value.ToString());
+                selectedProcess = Process.GetProcessById(processId);
+
+                //textBox1.Text = $"ID: {selectedProcess.Id}, Nombre: {selectedProcess.ProcessName}";
+            }
+            else
+            {
+               
+                selectedProcess = null;
+                //textBox1.Text = String.Empty;
+            }
+        }
+
+        // Botón para salir de la aplicación
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
